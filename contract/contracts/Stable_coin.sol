612871d5accd30892cd8bce50338038ab50b2962
Ankit kumar <animusk7765@gmail.com>
2023-06-29 23:44:42 +0530

    refactoring

--- contract/contracts/Stable_coin.sol
+++ contract/contracts/Stable_coin.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.18;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
